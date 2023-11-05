1d1163d738d0f942dd749e6512baf80ce2434b93
Nagy Florian <nagyflorian07@gmail.com>
2023-11-05 12:01:42 +0100

    changed lasttime to 120 instead of 60

--- dwm.c
+++ dwm.c
@@ -1389,7 +1389,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 120))
 				continue;
 			lasttime = ev.xmotion.time;
 
